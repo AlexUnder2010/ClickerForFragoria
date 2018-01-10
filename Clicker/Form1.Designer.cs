a8610994b2a74b74bb1c43843b990eab2cdb1237
Alex Mikhailik <alex-under-2010@mail.ru>
2018-01-11 01:25:28 +0200

    Initial commit

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 400;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 500;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 5000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick_1);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 61000;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick_1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(194, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Shift + 3 (Pet gets XP automatically)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(207, 72);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Menu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 91);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Fragoria bot";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        public const int MouseeventfLeftdown = 0x02;
+        public const int MouseeventfLeftup = 0x04;
+        public const int MouseeventfRightdown = 0x08;
+        public const int MouseeventfRightup = 0x10;
+
+        public Form1()
+        {
+            InitializeComponent();
+            keybd_event(0x90, 0x45, 0x1, (UIntPtr) 0);
+            var hook = new Hook(0x90); //NumLock
+
+            hook.KeyPressed += _hook_KeyPressed;
+            hook.SetHook();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+        private void _hook_KeyPressed(object sender, KeyPressEventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            timer4.Enabled = false;
+            timer5.Enabled = false;
+            Visible = Visible;
+            Activate();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public void MouseLeftClick(int a, int b)
+        {
+            Cursor.Position = new Point(a, b);
+            mouse_event(MouseeventfLeftdown, 0, 0, 0, 0);
+            mouse_event(MouseeventfLeftup, 0, 0, 0, 0);
+        }
+
+        public void MouseRightClick(int a, int b)
+        {
+            Cursor.Position = new Point(a, b);
+            mouse_event(MouseeventfRightdown, 0, 0, 0, 0);
+            mouse_event(MouseeventfRightup, 0, 0, 0, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer4.Enabled = true;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            SendKeys.Send("{TAB}");
+            timer2.Enabled = true;
+            timer1.Enabled = false;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send("l");
+            timer3.Enabled = true;
+            timer2.Enabled = false;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            MouseLeftClick(958, 514);
+            timer1.Enabled = true;
+            timer3.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer4_Tick_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            timer4.Enabled = false;
+            if (checkBox1.Checked)
+                timer5.Enabled = true;
+            else
+                timer5.Enabled = false;
+        }
+
+        private void timer5_Tick_1(object sender, EventArgs e)
+        {
+            SendKeys.Send("+3");
+        }
+
+        public class Hook : IDisposable
+        {
+            private readonly int _key;
+            private readonly KeyboardHookProc _proc;
+            private IntPtr _hHook = IntPtr.Zero;
+
+            public Hook(int keyCode)
+            {
+                _key = keyCode;
+                _proc = HookProc;
+            }
+
+            public void Dispose()
+            {
+                UnHook();
+            }
+
+            public event KeyPressEventHandler KeyPressed;
+
+            public void SetHook()
+            {
+                var hInstance = LoadLibrary("User32");
+                _hHook = SetWindowsHookEx(WhKeyboardLl, _proc, hInstance, 0);
+            }
+
+            public void UnHook()
+            {
+                UnhookWindowsHookEx(_hHook);
+            }
+
+            private IntPtr HookProc(int code, IntPtr wParam, IntPtr lParam)
+            {
+                if (code >= 0 && wParam == (IntPtr) WhKeydown && Marshal.ReadInt32(lParam) == _key)
+                    if (KeyPressed != null)
+                        KeyPressed(this, new KeyPressEventArgs(Convert.ToChar(code)));
+
+                return CallNextHookEx(_hHook, code, (int) wParam, lParam);
+            }
+
+            private delegate IntPtr KeyboardHookProc(int code, IntPtr wParam, IntPtr lParam);
+
+            #region Declare WinAPI functions
+
+            [DllImport("kernel32.dll")]
+            private static extern IntPtr LoadLibrary(string lpFileName);
+
+            [DllImport("user32.dll")]
+            private static extern IntPtr SetWindowsHookEx(int idHook, KeyboardHookProc callback, IntPtr hInstance,
+                uint threadId);
+
+            [DllImport("user32.dll")]
+            private static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, int wParam, IntPtr lParam);
+
+            [DllImport("user32.dll")]
+            private static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+
+            #endregion
+
+            #region Constants
+
+            private const int WhKeyboardLl = 13;
+            private const int WhKeydown = 0x0100;
+
+            #endregion
+        }
+    }
+}
